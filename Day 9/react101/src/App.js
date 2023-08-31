509d103ae87513f4702193e3a06e469c7af0bb26
Yash <naravade.11809605@lpu.in>
2023-08-31 10:41:28 +0530

    first react app in GAVS

---
+++ Day 9/react101/src/App.js
@@ -0,0 +1,16 @@
+import Logo from './logo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+  <br />
+  <br /><br />
+      <img src={Logo} alt="Logo" />
+      <h1>React 101</h1>
+      <p>My first React app</p>
+    </div>
+  );
+}
+
+export default App;
