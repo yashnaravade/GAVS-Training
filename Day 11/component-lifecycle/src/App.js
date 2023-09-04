a5e59f8fd81af42857f353fef30aa3ce9335ff35
Yash <naravade.11809605@lpu.in>
2023-09-04 14:26:01 +0530

    Colour change 3

--- Day 11/component-lifecycle/src/App.js
+++ Day 11/component-lifecycle/src/App.js
@@ -1,12 +1,14 @@
-import ChangePageColor from "./components/ChangePageColor";
-import ChangePageColorTwo from "./components/ChangePgColorTwo";
+// import ChangePageColor from "./components/ChangePageColor";
+// import ChangePageColorTwo from "./components/ChangePgColorTwo";
+import ChangePageColorThree from "./components/ChangeColorThree";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       {/* <ChangePageColor /> */}
-      <ChangePageColorTwo />
+      {/* <ChangePageColorTwo /> */}
+      <ChangePageColorThree />
     </div>
   );
 }
--- Day 11/component-lifecycle/src/components/ChangeColorThree.js
+++ Day 11/component-lifecycle/src/components/ChangeColorThree.js
@@ -1,37 +1,53 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class ChangePageColor extends Component {
   constructor(props) {
     super(props);
+
     this.state = { color: "blue" };
   }
 
   changeColor = (color) => {
     this.setState({ color });
+  };
+
+  componentDidMount() {
+    const dv = document.getElementById("mnt");
+
+    dv.innerHTML += "<br><b><u>Component mounted</u></b>";
+
+    dv.style.color = this.state.color;
   }
 
-  applyColor = () => {
+  componentDidUpdate() {
     const dv = document.getElementById("mnt");
+
+    dv.innerHTML += "<br><b><i>Component updated</b></i>";
+
     dv.style.color = this.state.color;
   }
 
   render() {
     return (
       <div>
-        <div id="mnt">
-          <b>In Updating phase</b><br /><br />
-        </div>
         <select
           value={this.state.color}
-          onChange={(e) => this.changeColor(e.target.value)}
+          onChange={(e) => {
+            this.changeColor(e.target.value);
+          }}
         >
           <option value="red">Red</option>
-          <option value="yellow">Yellow</option>
+
+          <option value="green">Green</option>
+
           <option value="brown">Brown</option>
+
           <option value="pink">Pink</option>
+
           <option value="teal">Teal</option>
         </select>
-        <button onClick={this.applyColor}>Apply Color</button>
+
+        <div id="mnt"></div>
       </div>
     );
   }
