8166574693abc1108a077740e2638659ae3842a5
Yash <naravade.11809605@lpu.in>
2023-09-04 15:56:04 +0530

    React router

--- Day 11/component-lifecycle/src/App.js
+++ Day 11/component-lifecycle/src/App.js
@@ -1,6 +1,8 @@
-// import ChangePageColor from "./components/ChangePageColor";
-// import ChangePageColorTwo from "./components/ChangePgColorTwo";
+import ChangePageColor from "./components/ChangePageColor";
+import ChangePageColorTwo from "./components/ChangePgColorTwo";
 import ChangePageColorThree from "./components/ChangeColorThree";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+
 import "./App.css";
 
 function App() {
@@ -8,7 +10,16 @@ function App() {
     <div className="App">
       {/* <ChangePageColor /> */}
       {/* <ChangePageColorTwo /> */}
-      <ChangePageColorThree />
+      {/* <ChangePageColorThree />  */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<ChangePageColor />} />
+          <Route path="/two" element={<ChangePageColorTwo />} />
+          <Route path="/three" element={<ChangePageColorThree />} />
+        </Routes>
+      </Router>
+      
+        
     </div>
   );
 }
