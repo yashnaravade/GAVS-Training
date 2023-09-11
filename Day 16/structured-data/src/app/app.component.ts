3658f9c015c8314aed1229aef38a1ad5e9857ad1
Yash <naravade.11809605@lpu.in>
2023-09-11 12:10:23 +0530

    Add form to push data into the table and add styling

--- Day 16/structured-data/src/app/app.component.ts
+++ Day 16/structured-data/src/app/app.component.ts
@@ -3,23 +3,87 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'structured-data';
 
-  emp={firstName: "Yash", lastName: "Naravade", age : 22, salary : 50000, designation : "Software Engineer", location : "Pune"};
+
+  firstName = '';
+  lastName = '';
+  age = 0;
+  salary = 0;
+  designation = '';
+  location = '';
 
   // declare an array of objects
   employees = [
-    {firstName: "Yash", lastName: "Naravade", age : 22, salary : 50000, designation : "Software Engineer", location : "Pune"},
-    {firstName: "Ankit", lastName: "Kumar", age : 25, salary : 60000, designation : "Senior Software Engineer", location : "Mumbai"},
-    {firstName: "Vansh", lastName: "Devgan", age : 30, salary : 70000, designation : "Team Lead", location : "Pune"},
-    {firstName: "Gaurav", lastName: "Saxena", age : 35, salary : 80000, designation : "Project Manager", location : "Bangalore"},
-    {firstName: "Ayushi", lastName: "Vaishnav", age : 40, salary : 90000, designation : "Project Manager", location : "Pune"},
-    {firstName: "Sarthak", lastName: "Garg", age : 45, salary : 100000, designation : "Project Manager", location : "Mumbai"},
-    {firstName: "Niraj", lastName: "Sahu", age : 50, salary : 110000, designation : "Project Manager", location : "Pune"}
+    {
+      firstName: 'Yash',
+      lastName: 'Naravade',
+      age: 22,
+      salary: 50000,
+      designation: 'Software Engineer',
+      location: 'Pune',
+    },
+    {
+      firstName: 'Ankit',
+      lastName: 'Kumar',
+      age: 25,
+      salary: 60000,
+      designation: 'Senior Software Engineer',
+      location: 'Mumbai',
+    },
+    {
+      firstName: 'Vansh',
+      lastName: 'Devgan',
+      age: 30,
+      salary: 70000,
+      designation: 'Team Lead',
+      location: 'Pune',
+    },
+    {
+      firstName: 'Gaurav',
+      lastName: 'Saxena',
+      age: 35,
+      salary: 80000,
+      designation: 'Project Manager',
+      location: 'Bangalore',
+    },
+    {
+      firstName: 'Ayushi',
+      lastName: 'Vaishnav',
+      age: 40,
+      salary: 90000,
+      designation: 'Project Manager',
+      location: 'Pune',
+    },
+    {
+      firstName: 'Sarthak',
+      lastName: 'Garg',
+      age: 45,
+      salary: 100000,
+      designation: 'Project Manager',
+      location: 'Mumbai',
+    },
+    {
+      firstName: 'Niraj',
+      lastName: 'Sahu',
+      age: 50,
+      salary: 110000,
+      designation: 'Project Manager',
+      location: 'Pune',
+    },
   ];
 
-    
+  addEmployee() {
+    this.employees.push({
+      firstName: this.firstName,
+      lastName: this.lastName,
+      age: this.age,
+      salary: this.salary,
+      designation: this.designation,
+      location: this.location,
+    });
+  }
 }
