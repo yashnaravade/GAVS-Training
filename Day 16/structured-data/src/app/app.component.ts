21455ae32ad9f54dc3b9290a26512425f087d027
Yash <naravade.11809605@lpu.in>
2023-09-11 10:20:06 +0530

    Display data in a table

--- Day 16/structured-data/src/app/app.component.ts
+++ Day 16/structured-data/src/app/app.component.ts
@@ -9,5 +9,17 @@ export class AppComponent {
   title = 'structured-data';
 
   emp={firstName: "Yash", lastName: "Naravade", age : 22, salary : 50000, designation : "Software Engineer", location : "Pune"};
-  
+
+  // declare an array of objects
+  employees = [
+    {firstName: "Yash", lastName: "Naravade", age : 22, salary : 50000, designation : "Software Engineer", location : "Pune"},
+    {firstName: "Ankit", lastName: "Kumar", age : 25, salary : 60000, designation : "Senior Software Engineer", location : "Mumbai"},
+    {firstName: "Vansh", lastName: "Devgan", age : 30, salary : 70000, designation : "Team Lead", location : "Pune"},
+    {firstName: "Gaurav", lastName: "Saxena", age : 35, salary : 80000, designation : "Project Manager", location : "Bangalore"},
+    {firstName: "Ayushi", lastName: "Vaishnav", age : 40, salary : 90000, designation : "Project Manager", location : "Pune"},
+    {firstName: "Sarthak", lastName: "Garg", age : 45, salary : 100000, designation : "Project Manager", location : "Mumbai"},
+    {firstName: "Niraj", lastName: "Sahu", age : 50, salary : 110000, designation : "Project Manager", location : "Pune"}
+  ];
+
+    
 }
