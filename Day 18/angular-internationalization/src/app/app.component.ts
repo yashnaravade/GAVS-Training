0d8af94da4c2e03db1bfe81845104bb227028383
Yash <naravade.11809605@lpu.in>
2023-09-13 16:31:32 +0530

    add french and english greeting using i18n

--- Day 18/angular-internationalization/src/app/app.component.ts
+++ Day 18/angular-internationalization/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>{{ 'GREETING' | translate }}</h1>
+    <button (click)="switchLanguage('en')">English</button>
+    <button (click)="switchLanguage('fr')">French</button>
+  `,
 })
 export class AppComponent {
-  title = 'angular-internationalization';
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('en'); // Default language
+  }
+
+  switchLanguage(language: string) {
+    this.translate.use(language);
+    console.log(`Switching to language: ${language}`);
+  }
 }
--- Day 18/angular-internationalization/src/app/app.module.ts
+++ Day 18/angular-internationalization/src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader'; // required for AOT compilation
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
