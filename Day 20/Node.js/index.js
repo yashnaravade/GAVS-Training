28e3703cc799ba9325b065a09f5dbe7018fcd9be
Yash <naravade.11809605@lpu.in>
2023-09-15 10:30:05 +0530

    Node hello world'

---
+++ Day 20/Node.js/index.js
@@ -0,0 +1,11 @@
+let myName = "Yash";
+console.log(myName);
+
+//Q: Where should you use a semicolon and where you shouldn't?
+//A: You should use a semicolon after every statement. But, if you don't use it, JavaScript will automatically add it for you. But, it's a good practice to use it. Also, there are some cases where JavaScript won't add a semicolon for you. For example, if you have two statements on the same line, JavaScript won't add a semicolon between them. So, it's better to add it yourself. Also, if you are using ES6 modules, you don't need to add a semicolon after every statement. But, if you are not using ES6 modules, you should add a semicolon after every statement.
+
+process.stdout.write("Hello, Node.js!\n");
+
+console.log(myName.length);
+console.log(myName.toUpperCase());
+console.log(myName.charAt(1));
