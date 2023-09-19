8d3899f7fb47adf00836c63aa23fcf672ce890dc
Yash <naravade.11809605@lpu.in>
2023-09-19 11:25:08 +0530

    text interpolation

--- practice/practice/src/app/app.component.ts
+++ practice/practice/src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'practice';
+  title = 'GoGo Angular';
+
+  getMin(a: number, b: number) {
+    return Math.min(a, b);
+  }
+
+  getMax(a: number, b: number) {
+    return Math.max(a, b);
+  }
 }
