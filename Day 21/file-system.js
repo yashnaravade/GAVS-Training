0a8ae0f0751dca4811fc1bc0eb7e210a67787dfb
Yash <naravade.11809605@lpu.in>
2023-09-20 11:27:38 +0530

    create and read a directory

--- Day 21/file-system.js
+++ Day 21/file-system.js
@@ -59,7 +59,6 @@ import fs from "fs";
 //   });
 // });
 
-
 // Closing a File
 // var buf = new Buffer.alloc(1024); // create a buffer to hold the contents of the file before displaying it to the console. The buffer is an array of bytes, and the toString() method converts it to a string. The buffer is sized to hold the number of bytes returned by the read() method. If the file is larger than the buffer, only the first part of the file will be read. If the file is smaller than the buffer, the entire file will be read. The read() method returns the number of bytes read. The close() method closes the file. The callback function is called when the file is closed. If an error occurs, the error parameter will contain the error code, and the data parameter will be undefined. If there is no error, the error parameter will be null, and the data parameter will contain the contents of the file. The data is displayed to the console.
 
@@ -95,40 +94,71 @@ import fs from "fs";
 // });
 
 // Truncate a File
-console.log("Going to open an existing file");
-fs.open("data2.txt", "r+", function (err, fd) {
+// console.log("Going to open an existing file");
+// fs.open("data2.txt", "r+", function (err, fd) {
+//   if (err) {
+//     return console.error(err);
+//   }
+//   console.log("File opened successfully!");
+//   console.log("Going to truncate the file after 10 bytes");
+
+//   // Truncate the opened file.
+//   let buf = new Buffer.alloc(1024);
+//   fs.ftruncate(fd, 10, function (err) {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log("File truncated successfully.");
+//     console.log("Going to read the same file");
+
+//     fs.read(fd, buf, 0, buf.length, 0, function (err, bytes) {
+//       if (err) {
+//         console.log(err);
+//       }
+
+//       // Print only read bytes to avoid junk.
+//       if (bytes > 0) {
+//         console.log(buf.slice(0, bytes).toString());
+//       }
+
+//       // Close the opened file.
+//       fs.close(fd, function (err) {
+//         if (err) {
+//           console.log(err);
+//         }
+//         console.log("File closed successfully.");
+//       });
+//     });
+//   });
+// });
+
+// Delete a File
+// console.log("Going to delete an existing file");
+// fs.unlink("data2.txt", function (err) {
+//   // The fs.unlink() method deletes the specified file
+//   if (err) {
+//     return console.error(err);
+//   }
+//   console.log("File deleted successfully!");
+// });
+
+// Create a Directory
+// console.log("Going to create directory /tmp/test");
+// fs.mkdir("./test", function (err) {
+//   if (err) {
+//     return console.error(err);
+//   }
+//   console.log("Directory created successfully!");
+// });
+
+// Read a Directory
+console.log("Going to read directory");
+fs.readdir("./", function (err, files) {
   if (err) {
     return console.error(err);
   }
-  console.log("File opened successfully!");
-  console.log("Going to truncate the file after 10 bytes");
-
-  // Truncate the opened file.
-  let buf = new Buffer.alloc(1024);
-  fs.ftruncate(fd, 10, function (err) {
-    if (err) {
-      console.log(err);
-    }
-    console.log("File truncated successfully.");
-    console.log("Going to read the same file");
-
-    fs.read(fd, buf, 0, buf.length, 0, function (err, bytes) {
-      if (err) {
-        console.log(err);
-      }
-
-      // Print only read bytes to avoid junk.
-      if (bytes > 0) {
-        console.log(buf.slice(0, bytes).toString());
-      }
-
-      // Close the opened file.
-      fs.close(fd, function (err) {
-        if (err) {
-          console.log(err);
-        }
-        console.log("File closed successfully.");
-      });
-    });
+  files.forEach(function (file) {
+    console.log(file);
+    // The fs.readdir() method takes a pathname as its first argument and a callback as its second. The callback signature is: (err, files) => {...}. After the method executes, the files parameter contains an array of all the file names in the directory.
   });
 });
