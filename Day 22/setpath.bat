32d57d86bf5d2d037c54b0b5a9f50597f821395e
Yash <naravade.11809605@lpu.in>
2023-09-21 12:48:59 +0530

    Login form with express

---
+++ Day 22/setpath.bat
@@ -0,0 +1,6 @@
+echo Modifying the path
+
+set path=%path%;C:\Users\govindan.k\AppData\Roaming\npm;C:\Users\govindan.k\AppData\Roaming\npm\node_modules\@angular\cli\bin;C:\Govindan\ExpressJS\node_modules\.bin
+
+
+echo Path modified
