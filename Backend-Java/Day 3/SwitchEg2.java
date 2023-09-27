1531938e6ef81e1821aa3b9df6d4bd802143c275
Yash <yashdip123@gmail.com>
2023-09-27 10:44:09 +0530

    Switch case with yeild and arrow

--- Backend-Java/Day 3/SwitchEg2.java
+++ Backend-Java/Day 3/SwitchEg2.java
@@ -3,19 +3,36 @@ public class SwitchEg2 {
     public static void main(String ar[]) {
 
         char c = ar[0].charAt(0);
+        String result;
 
-        switch (c) {
-            case 'a':
-            case 'e':
-            case 'i':
-            case 'o':
-            case 'u':
-                System.out.println("Vowel");
-                break;
-            default:
-                System.out.println("Consonant");
-        }
+        // switch (c) {
+        // case 'a':
+        // case 'e':
+        // case 'i':
+        // case 'o':
+        // case 'u':
+        // result = "Vowel";
+        // break;
+        // default:
+        // result = "Consonant";
+        // }
 
+        // with yield
+        // result = switch (c) {
+        //     case 'a', 'e', 'i', 'o', 'u' -> {
+        //         yield "Vowel";
+        //     }
+        //     default -> {
+        //         yield "Consonant";
+        //     }
+        // };
+
+        result = switch (c) {
+            case 'a', 'e', 'i', 'o', 'u' -> "Vowel";
+            default -> "Consonant";
+        };
+
+        System.out.println(result);
     }
 
 }
