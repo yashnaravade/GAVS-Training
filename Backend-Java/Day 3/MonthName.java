e33ee9ccdef8bdd2fb9561742199917273eafa4e
Yash <yashdip123@gmail.com>
2023-09-27 11:50:56 +0530

    Switch case practice problem

---
+++ Backend-Java/Day 3/MonthName.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class MonthName {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a month number (1-12): ");
+        int monthNumber = scanner.nextInt();
+        scanner.close();
+
+        String monthName;
+
+        switch (monthNumber) {
+            case 1:
+                monthName = "January";
+                break;
+            case 2:
+                monthName = "February";
+                break;
+            case 3:
+                monthName = "March";
+                break;
+            case 4:
+                monthName = "April";
+                break;
+            case 5:
+                monthName = "May";
+                break;
+            case 6:
+                monthName = "June";
+                break;
+            case 7:
+                monthName = "July";
+                break;
+            case 8:
+                monthName = "August";
+                break;
+            case 9:
+                monthName = "September";
+                break;
+            case 10:
+                monthName = "October";
+                break;
+            case 11:
+                monthName = "November";
+                break;
+            case 12:
+                monthName = "December";
+                break;
+            default:
+                monthName = "Invalid month";
+                break;
+        }
+
+        System.out.println("Month: " + monthName);
+    }
+}
