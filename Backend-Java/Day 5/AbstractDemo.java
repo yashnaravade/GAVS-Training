7a3bef80bacd6922d9e682404a424c0dafc20d2a
Yash <yashdip123@gmail.com>
2023-09-29 11:41:08 +0530

    Abstract and Inheritance

---
+++ Backend-Java/Day 5/AbstractDemo.java
@@ -0,0 +1,37 @@
+class Circle extends Shape{
+	
+	public Circle() {
+		// super("Blue");
+		System.out.println("Circle Created");
+	}
+	public Circle(String color) {
+		super(color);
+		System.out.println("Circle created with Color");
+	}
+	public void draw() {
+		System.out.println("Drawing a Circle");
+	}
+	// Book b=new Book();
+}
+class Rectangle extends Shape{
+	public Rectangle() {
+		System.out.println("Rectangle Created");
+	}
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		System.out.println("Drawing a Rectangle"); 
+	}
+	public int area(int length,int breadth) {
+		return length*breadth;
+	}
+}
+public class AbstractDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Circle myCircle=Circle();
+		myCircle.draw();
+	}
+
+}
---
+++ Backend-Java/Day 5/Shape.java
@@ -0,0 +1,20 @@
+
+public class Shape {
+	public Shape() {
+		System.out.println("Shape created");
+	}
+	public Shape(String color) {
+		this.color=color;
+		System.out.println("Shape with Color created");
+	}
+	private String color;
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+	// public abstract void draw();
+}
