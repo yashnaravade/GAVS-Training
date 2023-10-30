2bc129ae39b56eab05c7062255ca91c5086d59e9
Yash <yashdip123@gmail.com>
2023-10-30 15:02:47 +0530

    Login backend frontend connection

--- Backend-Java/Project/pharma-frontend/src/Components/Login/Login.js
+++ Backend-Java/Project/pharma-frontend/src/Components/Login/Login.js
@@ -1,15 +1,43 @@
 import React, { useState } from "react";
+import Footer from "../Footer/Footer";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [loginStatus, setLoginStatus] = useState(""); // To store the login status message
 
   const handleLogin = (e) => {
     e.preventDefault();
+
+    const user = {
+      username: username,
+      password: password,
+    };
+
+    axios
+      .post("http://localhost:6969/login", user)
+      .then((response) => {
+        if (response.data === "Success") {
+          // Update the state to indicate a successful login
+          setLoginStatus("Login successful!");
+        } else {
+          // Update the state to show an error message
+          setLoginStatus("Invalid credentials. Please try again.");
+        }
+      })
+      .catch((error) => {
+        console.error("An error occurred:", error);
+      });
+
+    setUsername("");
+    setPassword("");
   };
 
   return (
     <div className="login">
+      <br />
       <form onSubmit={handleLogin}>
         <div className="form-group">
           <label htmlFor="username">Username:</label>
@@ -34,12 +62,17 @@ const Login = () => {
         <button type="submit" className="cta-button">
           Login
         </button>
-
-        <p>
-          Don't have an account? <a href="/register">Register</a>
-        </p>
-
+        
+      <p>
+        Don't have an account? <Link to="/register">Register</Link>
+      </p>
       </form>
+
+      {/* Display the login status */}
+      {loginStatus && <p>{loginStatus}</p>}
+
+      <br />
+      <Footer />
     </div>
   );
 };
---
+++ Backend-Java/Project/pharma-frontend/src/Components/Register/Register.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import "./Register.css";
+
+const Register = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add your registration logic here
+    console.log(formData); // Replace this with your registration API call
+  };
+
+  return (
+    <div className="register-container">
+      <h2>Register</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="confirmPassword">Confirm Password</label>
+          <input
+            type="password"
+            name="confirmPassword"
+            value={formData.confirmPassword}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
